670bb673f80b4108423b6234d8ba7e6cea7bedbc
Caio Mansho <caiomansho@gmail.com>
2025-12-08 21:26:21 -0300

    initial commit

---
+++ app/src/main/java/com/example/caiomansho/AndroidApp.kt
@@ -0,0 +1,12 @@
+package com.example.caiomansho
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+///////////////////////////////////////////////////////
+//                                                   //
+//              DO NOT MODIFY THIS FILE              //
+//                                                   //
+///////////////////////////////////////////////////////
+@HiltAndroidApp
+class AndroidApp : Application()
